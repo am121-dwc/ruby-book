907fd6e09bad8376883a5a287789cdfb25204110
am121-dwc <150588833+am121-dwc@users.noreply.github.com>
2024-05-10 22:01:28 +0900

    save

--- class.rb
+++ class.rb
@@ -165,3 +165,170 @@
 # Product.freeze
 # p Product::DEFAULT_PRICE = 5000
 
+# class Product
+#   # クラスインスタンス変数
+#   @name = 'Product'
+#   def self.name
+#     # クラスインスタンス変数
+#     @name
+#   end
+#   def initialize(name)
+#     # インスタンス変数
+#     @name = name
+#   end
+#   def name
+#     # インスタンス変数
+#     @name
+#   end
+# end
+
+# p Product.name
+
+# product = Product.new('A great movie')
+# p Product.name
+# p product.name
+
+# class Product
+#   @@name = 'Product'
+#   def self.name
+#     @@name
+#   end
+#   def initialize(name)
+#     @@name = name
+#   end
+#   def name
+#     @@name
+#   end
+# end
+# class DVD < Product
+#   @@name = 'DVD'
+#   def self.name
+#     @@name
+#   end
+#   def upcase_name
+#     @@name.upcase
+#   end
+# end
+
+# # DVDクラスを定義したタイミングで@@nameが"DVD"に変更される
+# p Product.name
+# p DVD.name
+
+# product = Product.new('A great movie')
+# p product.name
+
+# # Product.newのタイミングで@@nameが"A great movie"に変更される
+# p Product.name
+# p DVD.name
+
+# dvd = DVD.new('An awesome movie')
+# p dvd.name
+# p dvd.upcase_name
+
+# # DVD.newのタイミングで@@nameが"An awesome film"に変更される
+# p product.name
+# p Product.name
+# p DVD.name
+
+# グローバル変数の宣言と値の代入
+# $program_name = 'Awesome program'
+
+# # グローバル変数に依存するクラス
+# class Program
+#   def initialize(name)
+#     $program_name = name
+#   end
+
+#   def self.name
+#     $program_name
+#   end
+
+#   def name
+#     $program_name
+#   end
+# end
+
+# # $program_nameにはすでに名前が代入されている。
+# p Program.name
+
+# p program = Program.new('Super program')
+# p program.name
+
+# # Program.newのタイミングで$program_nameが"Super program"に変更される
+# p Program.name
+# p $program_name
+
+# class User
+#   def hello
+#     'hello'
+#   end
+
+#   # helloメソッドのエイリアスメソッドとしてgreetingを定義する
+#   alias greeting hello
+# end
+# user = User.new
+# p user.hello
+# p user.greeting
+
+# class User
+#   # freezeメソッドの定義を削除する
+#   undef freeze
+# end
+# user = User.new
+# # freezeメソッドを呼び出すとエラーになる
+# user.freeze
+
+# class User
+#   class BloodType
+#     attr_reader :type
+#     def initialize(type)
+#       @type = type
+#     end
+#   end
+# end
+# blood_type = User::BloodType.new('B')
+# p blood_type.type
+
+# class User
+#   # =で終わるメソッドを定義する
+#   def name=(value)
+#     @name = value
+#   end
+# end
+# user = User.new
+# # 変数に代入するような形式でname=メソッドを呼び出される
+# user.name = 'Alice'
+# p user
+
+# class Product
+#   attr_reader :code, :name
+
+#   def initialize(code, name)
+#     @code = code
+#     @name = name
+#   end
+#   def ==(other)
+#     if other.is_a?(Product)
+#       # 商品コードが一致すれば同じProductとみなす。
+#       code == other.code
+#     else
+#       # otherがProductでなければ常にfalse
+#       false
+#     end
+#   end
+# end
+
+# # aとcが同じ商品コード
+# a = Product.new('A-0001','A great movie')
+# b = Product.new('B-0001','An awesome file')
+# c = Product.new('A-0001','A great movie')
+
+# # ==がこのように動作して欲しい
+# p a == b #false
+# # ==メソッドを再定義しているのtrue
+# p a == c #true
+# p a == 1
+# p a.== 'a'
+# # 左辺にあるのが整数なので、Integerクラスの==が呼び出される
+# p 1== a
+
