11cc08df57c6ce347fe6dc3f204ef28df43a32b0
am121-dwc <150588833+am121-dwc@users.noreply.github.com>
2024-05-25 18:47:49 +0900

    save

--- sample.rb
+++ sample.rb
@@ -284,19 +284,19 @@
 # # parse(=解析するの意味JSONを解析する)
 # p JSON.parse(user_json)
 
-require 'yaml'
+# require 'yaml'
 
-# YAML形式のテキストデータを用意する
-yaml = <<TEXT
-alice:
-  name: 'Alice'
-  email: 'exampledotco'
-  age: 20
-TEXT
-# YAMLテキストをパースしてハッシュに変換する
-users = YAML.load(yaml)
+# # YAML形式のテキストデータを用意する
+# yaml = <<TEXT
+# alice:
+#   name: 'Alice'
+#   email: 'exampledotco'
+#   age: 20
+# TEXT
+# # YAMLテキストをパースしてハッシュに変換する
+# users = YAML.load(yaml)
 
-# ハッシュに新しい要素を追加する
-users['alice']['gender'] = :female
-# ハッシュからYAMLテキストに変換する
-puts YAML.dump(users)
+# # ハッシュに新しい要素を追加する
+# users['alice']['gender'] = :female
+# # ハッシュからYAMLテキストに変換する
+# puts YAML.dump(users)
